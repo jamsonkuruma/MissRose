18537de8880c9d99bfafdaf65d62588c64a70317
jamsonkuruma <85690562+jamsonkuruma@users.noreply.github.com>
2021-06-24 12:41:07 +0530

    Update MissRoseSong_Bot.py

--- MissRoseSong_Bot.py
+++ MissRoseSong_Bot.py
@@ -29,7 +29,7 @@ def time_to_seconds(time):
 
 
 
-@bot.on_message(filters.command(['a']))
+@bot.on_message(filters.command(['song']))
 def a(client, message):
     query = ''
     for i in message.command[1:]:
